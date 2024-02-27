785dfd57917660e2ff63f3fc8c152859e0522f03
Kutlwano Modutsoane <kutlwano.modutsoane@gmail.com>
2024-02-28 01:02:32 +0200

    Tests were added in client_tests.py

--- client_test.py
+++ client_test.py
@@ -8,6 +8,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    stock, bid_price,ask_price, price = getDataPoint(quotes[0])
+    self.assertEqual(stock, 'ABC')
+    self.assertEqual(bid_price, 120.48)
+    self.assertEqual(ask_price, 121.2)
+    self.assertEqual(price, (120.48 + 121.2) / 2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,7 +20,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    stock, bid_price,ask_price, price = getDataPoint(quotes[0])
+    self.assertEqual(stock, 'ABC')
+    self.assertEqual(bid_price, 120.48)
+    self.assertEqual(ask_price, 119.2)
+    self.assertEqual(price, (120.48 + 119.2) / 2)
 
   """ ------------ Add more unit tests ------------ """
 
